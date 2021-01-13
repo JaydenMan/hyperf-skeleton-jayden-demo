cb1fd2cc2e14b6d68eebe9290ab098d47b6965e7
Jcai1231 <603480498@qq.com>
2021-01-13 10:29:07 +0800

    a

--- app/Controller/TcpServer.php
+++ app/Controller/TcpServer.php
@@ -8,9 +8,11 @@ use Hyperf\Contract\OnReceiveInterface;
 
 class TcpServer implements OnReceiveInterface
 {
+
     public function onReceive($server, int $fd, int $fromId, string $data): void
     {
         echo 'TCP request data:', $data;
         $server->send($fd, 'hyperf has reveived data:' . $data);
     }
+
 }
